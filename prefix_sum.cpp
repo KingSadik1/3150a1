6c2d6de51f1a1397a3bec197c070819bb9d91ece
sck20002 <sck20002@sck20002-vm.cse.uconn.edu>
2023-01-26 19:17:26 -0500

    initial commit

--- prefix_sum.cpp
+++ prefix_sum.cpp
@@ -1,32 +1,33 @@
+
 #include <iostream>
 #include "prefix_sum.h"
 
-int prefix_sum(int array[], int last){
-    int sum = 0;
-    for(int i =0; i < last; i++ ){
-        sum += array[i];
+int prefix_sum(int array[], int last){  
+ int sum = 0;
+ for(int i =0; i < last; i++ ){
+       sum = sum +  array[i];
     }
     return sum;
 }
 
 bool non_negative_prefix_sum(int array[], int length){
-    bool is_non_negative = false;
-    for(int i = 0; i < length; i++){
-        int sum = prefix_sum(array, i);
-        if(sum >= 1){
-            is_non_negative = true;
-            break;
+ bool is_non_negative = true;
+ for(int i = 0; i <= length;i++){
+        int sum = prefix_sum(array , i);
+        if(sum <= -1){
+          is_non_negative = false;
+	  break;
         }
     }
     return is_non_negative;
 }
 
 bool non_positive_prefix_sum(int array[], int length){
-    bool is_non_positive = false;
-    for(int i = 0; i < length; i++){
-        int sum = prefix_sum(array, i);
-        if(sum < 0){
-            is_non_positive = true;
+    bool is_non_positive = true;
+    for(int i = 0; i <= length; i++){
+        int sum =  prefix_sum(array, i);
+        if(sum > 0){
+            is_non_positive = false;
             break;
         }
     }
